0863d4df268e74941459fc32a418bdec48829f78
2H-STORMMING <94780969+2H-STORMMING@users.noreply.github.com>
2023-10-13 23:04:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -9,16 +9,30 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H1(children='Pink Morsel price increase impact', style={'textAlign':'center'}),
     dcc.Dropdown(df.region.unique(), 'south', id='dropdown-selection'),
-    dcc.Graph(id='graph-content')
+    dcc.Graph(id='graph-content'),
+    dcc.Dropdown(df.region.unique(), 'south', id='dropdown-selection1'),
+    dcc.Graph(id='graph1-content')
 ])
 
 @callback(
     Output('graph-content', 'figure'),
     Input('dropdown-selection', 'value')
 )
+
 def update_graph(value):
-   # dff = df[df.country==value]
-    return px.line(df, x='date', y='sales')
+    dff = df[df.region==value]
+    return px.line(dff, x='date', y='sales')
+
+
+@callback(
+    Output('graph1-content', 'figure'),
+    Input('dropdown-selection1', 'value')
+)
+
+
+def update_graph1(value):
+    dff = df[df.region==value]
+    return px.line(dff, x='date', y='quantity')
 
 if __name__ == '__main__':
     app.run(debug=True)
