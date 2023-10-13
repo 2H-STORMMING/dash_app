292c106087b73a04858a865a4890be3f8c35dfef
2H-STORMMING <94780969+2H-STORMMING@users.noreply.github.com>
2023-10-13 19:16:59 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+df = pd.read_csv(r'C:\Users\pc\Desktop\projects\python\candy_app\candy\quantium-starter-repo\pink_morsel.csv')
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
-    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    html.H1(children='Pink Morsel price increase impact', style={'textAlign':'center'}),
+    dcc.Dropdown(df.region.unique(), 'south', id='dropdown-selection'),
     dcc.Graph(id='graph-content')
 ])
 
@@ -17,8 +17,8 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_graph(value):
-    dff = df[df.country==value]
-    return px.line(dff, x='year', y='pop')
+   # dff = df[df.country==value]
+    return px.line(df, x='date', y='sales')
 
 if __name__ == '__main__':
     app.run(debug=True)
